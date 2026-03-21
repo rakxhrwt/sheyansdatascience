f254fd41bb6018ae14301a927ea39e0e87816554
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-03-21 17:48:13 +0530

    f3

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+def BinarySearch(lst, target):
+    i = 0
+    j = len(lst) - 1
+
+    while i <= j:
+        mid = (i + j) // 2
+
+        if lst[mid] == target:
+            return True
+        
+        elif lst[mid] < target:
+            i = mid + 1
+        
+        else:
+            j = mid - 1
+
+    return False
+print(BinarySearch([1,2,3,4,5,6],7))
--- fucntionlogic2.py
+++ fucntionlogic2.py
@@ -1,3 +1,6 @@
+# we use return statem for send data back to where function will be called
+
 def add_numbers(a, b):
     # Write your code here
     return a+b
+
